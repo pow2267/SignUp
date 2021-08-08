9cf245cf93a9918141d6b3c0c23c5159568ee725
pow2267 <pow2267@gmail.com>
2021-08-08 20:18:50 +0900

    Upgrade codes by using review

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -22,14 +22,20 @@ class ViewController: UIViewController {
 
     // UserInformation에 저장된 정보가 있으면 id field에 채워줌
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
+        // review: viewWillAppear(_ animated: Bool)의 animated는 현재 ViewController가 화면에 나타나기 전에 animation과 함께 나타나는 지를 알려주는 정보에 해당합니다.
+        // 임의로 true로 변경하여 전달하는 것보다는 해당 정보를 포함하여 super에 넘겨주는 것이 올바른 방법입니다.
+        super.viewWillAppear(animated)
         
-        guard let id = UserInformation.shared.id else {
-            self.idField.text = ""
-            return
-        }
+        // review: self.idField.text = UserInformation.shared.id 와 같이 간결하게 작성 가능합니다.
+        // (두 값 모두 옵셔널이므로)
+        self.idField.text = UserInformation.shared.id
         
-        self.idField.text = id
+//        guard let id = UserInformation.shared.id else {
+//            self.idField.text = ""
+//            return
+//        }
+//
+//        self.idField.text = id
     }
 }
 
