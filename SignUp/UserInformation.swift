14bd238ce1bd5437a368a55579884c5aa2fe81e6
pow2267 <pow2267@gmail.com>
2021-08-08 20:49:25 +0900

    Upgrade codes by using review

--- SignUp/UserInformation.swift
+++ SignUp/UserInformation.swift
@@ -11,6 +11,10 @@ import UIKit
 class UserInformation {
     static let shared: UserInformation = UserInformation()
     
+    // review: 싱글턴 클래스는 보통 내부에서 생성한 static let 프로퍼티를 제외하고 외부에서 생성하지 못하는 것이 원칙이므로,
+    // 생성자를 private으로 선언하여 외부에서 생성되지 못하도록 막는 것이 일반적입니다.
+    private init() {}
+    
     var id: String?
     var password: String?
     var image: UIImage?
